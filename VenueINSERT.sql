96f83dfc5977148a300e9a648374d958af78cbeb
UP879389 <44237869+UP879389@users.noreply.github.com>
2020-11-02 17:36:39 +0000

    Add files via upload

---
+++ VenueINSERT.sql
@@ -0,0 +1,99 @@
+CREATE TABLE Venue (
+Venue_ID int primary key,
+Venue_Name varchar(30) not null,
+Venue_Street varchar(30) not null,
+Venue_City varchar(30) not null,
+Venue_PostCode varchar(15) not null,
+Venue_Phone varchar(20) not null,
+Venue_Email varchar(50) not null
+);
+
+-- INSERT INTO Venue VALUES(105,'The Pilgrim','St. Jamess', 'Bedfordshire','MK401QB','62233233328','Pilgri@gmial.com');
+-- INSERT INTO Venue VALUES(137,'The Crown','Canon Lane','Biggleswade','SG18 OJE','16362620078','Thecrown@gmail.com');
+-- INSERT INTO Venue VALUES(111,'The Gary Cooper','Waldram Park Road','Dunstable','LU5 4GP','62484521601','Thegarycooper@gmail.com');
+-- INSERT INTO Venue VALUES(185,'The White House','Heritage Tunnel' , 'Luton','LU1 1SA','8646373075','Thewhitehouse@gmail.com');
+-- INSERT INTO Venue VALUES(123,'The Swan','Caslon   Crossroad', 'Leighton','LU71EA','48018371830','Theswan@gmail.com');
+-- INSERT INTO Venue  VALUES(174,'The Bear','Apothecary Boulevard', 'Maidenhead','SL61QJ','73484533338','Thebear@gmail.com');
+-- INSERT INTO Venue VALUES(139,'The Hatchet','Dyott Lane','Newbury','RG14 5BD','53671278478','Thehatchet@gmail.com');
+
+-- INSERT INTO Venue VALUES(185,'The Hope Tap','Tiptree Rue', 'Reading','RG11EP','62745138604','Thehopetap@gmail.com');
+-- INSERT INTO Venue  VALUES(138,'The King and Castle','Vintners Grove', 'Windsor','SL41PL','04568342112','Thekingandcastle@gmail.com');
+-- INSERT INTO Venue  VALUES(153,'The Moon and Spoon','Charterhouse Avenue', 'Slough','SL11EL','54058275146','Themoonandspoon@gmail.com');
+-- INSERT INTO Venue  VALUES(119,'The Old Manor','Cavendish Grove', 'Bracknell','RG121BP','26265757738','Theoldmanor@gmail.com');
+-- INSERT INTO Venue  VALUES(140,'The Windlesora','Udall Avenue', 'Windsor','SL41BB','21266544067','Thewindlesora@gmail.com');
+-- INSERT INTO Venue  VALUES(167,'The Berkeley','Heritage Route', 'Bristol','BS81QE','58372753843','Theberkeley@gmail.com');
+-- INSERT INTO Venue  VALUES(111,'St Georges Hall','Camdale  Route', 'Bristol','BS59HL','46731233451','Stgeorgeshall@gmail.com');
+-- INSERT INTO Venue  VALUES(134,'The Commercial Room','Eldon  Way', 'Bristol','BS11HT','67706881028','Thecommercialroom@gmail.com');
+INSERT INTO Venue  VALUES(158,'The Bell','Canon Way', 'Aylesbury','HP2O1TX','25514246475','Thebell@gmail.com');
+
+
+INSERT INTO Venue  VALUES(204,'The Falcon','Gautrey  Boulevard', 'High Wycombe','HP112AX','81767404503','Thefalcon@gmail.com');
+
+INSERT INTO Venue  VALUES(133,'The Hope and Champion','Caroline Route', 'Beaconsfield','HP92SE','73582574062','Thehope@gmail.com');
+INSERT INTO Venue  VALUES(142,'The Moon Under Water','Beatty Lane', 'Milton Keynes','MK93NN','61435807610','Themoonunderwater@gmail.com');
+INSERT INTO Venue  VALUES(121,'The White Hart','Chesterfield  Alley', 'Aylesbury','HP2O1UR','41482515688','Thewhitehart@gmail.com');
+INSERT INTO Venue  VALUES(102,'Wetherspoons','Thorndike Street', 'Milton Keynes','MK91EA','27166572114','Wheterspoons@gmail.com');
+INSERT INTO Venue  VALUES(151,'Sandford House','Aspen Vale', 'Huntingdon','PE293AD','78853336576','Sandfordhouse@gmail.com');
+INSERT INTO Venue  VALUES(175,'The College Arms','West Grove', 'Peterborough','PE11RS','84708338466','Thecollegearms@gmail.com');
+INSERT INTO Venue  VALUES(200,'The Hippodrome','Queen Avenue', 'March','PE158AQ','32517062384','Thehippodrome@gmail.com');
+INSERT INTO Venue  VALUES(145,'The Regal','Belgrave  Pass', 'Cambridge','CB23AR','55656713423','Theregal@gmail.com');
+INSERT INTO Venue  VALUES(155,'The Weeping Ash','Addison Tunnel', 'St Neots','PE191AE','68131078163','Theweepingash@gmail.com');
+INSERT INTO Venue  VALUES(124,'The Wheatsheaf Inn ','Adams Drive', 'Wisbech','PE131BL','67227728405','Thewheatsheafinn@gmail.com');
+INSERT INTO Venue  VALUES(211,'The Calverts Court','Magnolia Way', 'Stockport','SK11EB','77842302233','Thecalberts@gmail.com');
+
+
+INSERT INTO Venue  VALUES(118,'The Counting House','Canning Vale', 'Congleton','CW121AH','67046827471','Thecountinghouse@gmail.com');
+INSERT INTO Venue  VALUES(146,'The Ferry Boat','Queen Tunnel', 'Runcorn','WA71LR','43611584083','Theferryboat@gmail.com');
+INSERT INTO Venue  VALUES(166,'The Kingfisher','Lake Route', 'Poynton','SK121NJ','30255846285','Thekingfisher@gmail.com');
+
+INSERT INTO Venue  VALUES(170,'The Looking Glass','Falconberg Drive', 'Warrington','WA12LY','64427716233','Thelookingglass@gmail.com');
+
+
+INSERT INTO Venue  VALUES(186,'The Penny Black','Cingworth Tunnel', 'Northwich','CW95AB','84587232486','Thepennyblack@gmail.com');
+
+INSERT INTO Venue  VALUES(108,'The Premier ','Collingwood Avenue', 'Widnes','WA86JS','14424267043','Thepremier@gmail.com');
+INSERT INTO Venue  VALUES(180,'The Unicorn','East Road', 'Altrincham','WA142DP','17213581616','Theunicorn@gmail.com');
+INSERT INTO Venue  VALUES(165,'The Company Row','Birkenhead Rue', 'Consett','DH85BQ','78127175062','Thecompanyrow@gmail.com');
+INSERT INTO Venue  VALUES(182,'The Five Quarter','Amwell Lane', 'Peterlee','SR81AB','68434212424','Thefivequarter@gmail.com');
+INSERT INTO Venue  VALUES(171,'The Ward Jackson','Babmaes Alley', 'Hartelpool','TS247EU','46766313783','Thewardjackson@gmail.com');
+INSERT INTO Venue  VALUES(164,'The William Stead','Gautrey Walk', 'Darlington','DL11LU','13562730560','thewilliamstead@gmail.com');
+INSERT INTO Venue  VALUES(152,'The Bransty Arch','Egerton Vale', 'Whitehaven','CA287XE','43872305526','Thebranstyarch@gmail.com');
+INSERT INTO Venue  VALUES(110,'The Furness Railway','Gavel Pass', 'Barrow-in-Furness','LA145UB','45686806605','Thefurnessrailway@gmail.com');
+INSERT INTO Venue  VALUES(149,'The Henry Bessemer','Canon Lane', 'Workington','CA142NA','80316556161','Thehenrybessemer@gmail.com');
+INSERT INTO Venue  VALUES(162,'The William Rufus','Clere Crossroad', 'Carlisle','CA11QS','77572670645','Thewilliamrufus@gmail.com');
+
+INSERT INTO Venue  VALUES(208,'Spa Lane Vaults','Coborn Drive', 'Chesterfield','S417TH','40231128221','Spalanevaults@gmail.com');
+
+INSERT INTO Venue  VALUES(135,'The Babington Arms','Gavel Alley', 'Derby','DE11TA','12547071756','Thebabingtonarms@gmail.com');
+
+INSERT INTO Venue  VALUES(147,'The Observatory','Chancellor Walk', 'Ilkeston','DE75QA','43501301878','Theobservatory@gmail.com');
+
+
+INSERT INTO Venue  VALUES(199,'The Pillar of Rock','Sherlock Vale', 'Chesterfield','S446PP','41182533215','Thepillarofrock@gmail.com');
+
+
+INSERT INTO Venue  VALUES(222,'The Red Lion','Elystan  Rue','Ripley','DE53BS','04640625608','Theredlion@gmail.com');
+
+
+INSERT INTO Venue  VALUES(109,'The Green Giner','Cave   Way', 'Torquay','TQ25DZ','20358526878','Thegreenginer@gmail.com');
+
+
+INSERT INTO Venue  VALUES(150,'The Imperial','Chamberlain Way', 'Exeter','EX44AH','87237626802','Theimperial@gmail.com');
+
+
+INSERT INTO Venue  VALUES(177,'The Isaac Merritt','Camdenhurst Road', 'Paignton','TQ33AA','82310271383','Theisaacmerritt@gmail.com');
+INSERT INTO Venue  VALUES(169,'The Star Inn','Andrews  Tunnel', 'Honiton','EX141BS','36456165234','Thestarinn@gmail.com');
+INSERT INTO Venue  VALUES(160,'The Greyhound','Armory Walk', 'Bridport','DT63LF','84450377562','Thegreyhound@gmail.com');
+
+INSERT INTO Venue  VALUES(220,'The Mary Shelly','Gate   Crossroad', 'Bournemouth','BH12AD','12534083302','Themaryshelly@gmail.com');
+INSERT INTO Venue  VALUES(115,'The Quay','Ely Grove', 'Poole','BH151HJ','56030141465','Thequay@gmail.com');
+INSERT INTO Venue  VALUES(178,'The Prior John','Gathorne   Tunnel', 'Bridlington','YO152QD','78753348121','Thepriorjohn@gmail.com');
+
+INSERT INTO Venue  VALUES(219,'The Barking Dog','Edwin Tunnel','Barking','IG118TU','37661448453','Thebarkingdog@gmail.com');
+
+
+INSERT INTO Venue  VALUES(210,'The Elms','Sundown Avenue', 'Leigh-on-sea','SS93ND','77503671305','Theelms@gmail.com');
+
+INSERT INTO Venue  VALUES(188,'The Playhouse','Coalecroft Vale', 'Colchester','CO27AA','02004486407','Theplayhouse@gmail.com');
+
+INSERT INTO Venue  VALUES(216,'The Wagon Works','Southampton road', 'Eastleigh','SO509FJ','963876432','Thewagonworks@gmail.com');
